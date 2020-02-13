4825dc945784f43c7c0d7d9df73229e66f624c05
anniephilip23 <60193502+anniephilip23@users.noreply.github.com>
2020-02-13 12:18:45 +0530

    Update 764.js

--- CodeKata/JavaScript/764.js
+++ CodeKata/JavaScript/764.js
@@ -1,1 +1,21 @@
 
+const readline = require('readline');
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+	userInput.push(data);
+});
+
+inp.on("close", () => {
+  var a, b;
+  [a, b] = userInput;
+  a1 = parseInt(a);
+  b1 = parseInt(b)
+  c = a1*b1;
+	console.log(c)
+});
