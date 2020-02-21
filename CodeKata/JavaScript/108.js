d4c93cdcda3a1a277881706da99a84d32cdaf0fb
anniephilip23 <60193502+anniephilip23@users.noreply.github.com>
2020-02-21 20:37:33 +0530

    Update 108.js

--- CodeKata/JavaScript/108.js
+++ CodeKata/JavaScript/108.js
@@ -1,1 +1,25 @@
 
+const readline = require('readline');
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+inp.on("line", (data) => {
+  var x =parseInt(data);
+
+  var array =[]
+  for(i=1;i<=x;i++){
+      var z= x%i
+    if(z==0){
+    array.push(i)}
+  }
+  var odd =[]
+  
+  for(j=0;j<array.length;j++){ 
+    var r= array[j]%2
+    if( r !=0){
+    odd.push(array[j])}
+  }
+  console.log(odd.join(" "))
+})
