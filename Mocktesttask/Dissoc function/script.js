16f36433bd17b74a39dc360551a051231b1f30e7
anniephilip23 <60193502+anniephilip23@users.noreply.github.com>
2020-05-04 17:19:29 +0530

    Update script.js

--- Mocktesttask/Dissoc function/script.js
+++ Mocktesttask/Dissoc function/script.js
@@ -2,14 +2,17 @@ function dissoc(a,b){
     var obj = b
     // getting keys from the input object
     var key = Object.keys(obj)
-    //ti chk input value is inkeys
-    for(i=0;i<key.length;i++){
-        var d = key[i]
-        if(d==a){
-            delete obj[d]
-        }
-    }
-    return obj
+    //to chk input value is inkeys
+    var arr  = key.filter(function(value)
+    {
+  return value!=a
+    })
+    var objNew ={}
+    var map = arr.forEach(function(value){
+        var x = obj[value]
+        objNew[value] =x
+    })
+    console.log(objNew)
 }
-
-console.log(dissoc("b", { a: 1, b: 2, c: 3 }))
+//console.log(
+    dissoc("b", { a: 1, b: 2, c: 3 })
